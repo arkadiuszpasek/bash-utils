cb62258ac18983eaf9cfe95056b80d7d0720a364
Arkadiusz Pasek <arkadiusz.pasek99@gmail.com>
2025-05-01 09:21:09 +0200

    Add git-purge

---
+++ sh/git-purge.sh
@@ -0,0 +1,18 @@
+# ~/.git-aliases.sh
+
+# git-purge - Delete local branches matching a pattern
+# Usage: git-purge <pattern1> [pattern2] [...]
+# Example: git-purge 'feature/.*' 'bugfix/.*'
+
+git-purge() {
+  if [ "$#" -eq 0 ]; then
+    echo "Usage: git-purge <pattern1> [pattern2] [...]"
+    return 1
+  fi
+
+  PATTERN=$(printf "%s|" "$@")
+  PATTERN=${PATTERN%|}
+
+  echo "Deleting branches matching: $PATTERN"
+  git branch | grep -E "$PATTERN" | xargs -r git branch -D
+}
